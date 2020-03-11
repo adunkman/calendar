609a2a716fd82107643932d1452ee56905f155c1
Andrew Dunkman <adunkman@gmail.com>
2020-03-10 23:09:28 -0400

    Configure for heroku deployment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,16 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-# Make sure a database connection can be made
-printf "Waiting for PostgreSQL ... "
+if [[ $RAILS_ENV != 'production' ]]; then
+  # Make sure a database connection can be made
+  printf "Waiting for PostgreSQL ... "
 
-while ! psql $DB_CONNECTION_STRING -c '\q'; do
-  sleep 0.1
-done
+  while ! psql $DATABASE_URL -c '\q'; do
+    sleep 0.1
+  done
 
-echo "done"
+  echo "done"
+fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
