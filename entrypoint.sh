a251b9cd6ba664d0011ac19e3f0dc269c3924242
Andrew Dunkman <adunkman@gmail.com>
2020-03-10 22:51:29 -0400

    Configure database.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,14 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
+# Make sure a database connection can be made
+printf "Waiting for PostgreSQL ... "
+
+while ! psql $DB_CONNECTION_STRING -c '\q'; do
+  sleep 0.1
+done
+
+echo "done"
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
